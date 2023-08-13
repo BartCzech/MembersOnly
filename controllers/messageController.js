4ce1f1c6bc8bb637c7579659433c3ba50cf12983
Bart <barteqczech@gmail.com>
2023-08-13 12:13:03 +0200

    Creating a message

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -3,8 +3,27 @@ const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 
 exports.create_message_get = asyncHandler(async (req, res) => {
-  res.render("create-message", { title: "Create a message" });
+  res.render("create-message", { title: "Create a message", user: res.locals.currentUser });
 });
+exports.create_message_post = [
+  body("title")
+  .trim()
+  .escape(),
+  body("text")
+  .trim()
+  .escape(),
+  asyncHandler(async (req, res) => {
+    const newMessage = new Message({
+      title: req.body.title,
+      text: req.body.text,
+      author: res.locals.currentUser,
+      timestamp: Date.now(),
+    });
+    await newMessage.save();
+    res.redirect("/messages");
+  }),
+];
+  
 
 exports.messages_get = asyncHandler(async (req, res) => {
   let allMessages = [];
@@ -14,8 +33,8 @@ exports.messages_get = asyncHandler(async (req, res) => {
     } else {
         allMessages = await Message.find().select("-author").exec();
     }
-    res.render("messages", { title: "Messages", messages: allMessages });
+    res.render("messages", { title: "Messages", messages: allMessages.reverse(), user: res.locals.currentUser });
   } else {
-    res.render("messages", { title: "Log in in order to see the messages." });
+    res.render("messages", { title: "Log in in order to see the messages.", user: res.locals.currentUser });
   }
 });
--- routes/messages.js
+++ routes/messages.js
@@ -4,6 +4,7 @@ const messageController = require('../controllers/messageController');
 
 /* GET messages listing. */
 router.get('/create-message', messageController.create_message_get);
+router.post('/create-message', messageController.create_message_post);
 router.get('/', messageController.messages_get);
 
 module.exports = router;
